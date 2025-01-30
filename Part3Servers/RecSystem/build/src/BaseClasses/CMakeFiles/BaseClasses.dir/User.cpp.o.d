ea20005a3e7f2b50543b46993fc19f0ec5ff7a4d
eyalb1380 <eyalb1380@gmail.com>
2025-01-30 21:01:32 +0200

    SCRUM-59 Commit with no build

--- Part3Servers/RecSystem/build/src/BaseClasses/CMakeFiles/BaseClasses.dir/User.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/BaseClasses/CMakeFiles/BaseClasses.dir/User.cpp.o: \
- /root/Netflix_part4-1/Part3Servers/RecSystem/src/BaseClasses/User.cpp \
- /usr/include/stdc-predef.h \
- /root/Netflix_part4-1/Part3Servers/RecSystem/src/BaseClasses/User.h
